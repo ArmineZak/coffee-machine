e7a36cab7a9b4be25ae3dae8172626b145457657
ArmineZak <armzak91@gmail.com>
2020-07-15 23:51:49 +0400

    - added scanner to input the amount of each ingredient containing in coffee machine

--- virtual-coffee-machine/src/EnoughCoffeeForEveryone.java
+++ virtual-coffee-machine/src/EnoughCoffeeForEveryone.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class EnoughCoffeeForEveryone {
+    public static void main(String[] args) {
+
+        int waterForOneCup = 200;
+        int milkForOneCup = 50;
+        int coffeeBeansForOneCup = 15;
+
+        Scanner scanWater = new Scanner(System.in);
+        System.out.print("Write how many ml of water the coffee machine has:\n" + "> ");
+        int waterAmount = scanWater.nextInt();
+
+        Scanner scanMilk = new Scanner(System.in);
+        System.out.print("\nWrite how many ml of milk the coffee machine has:\n" + "> ");
+        int milkAmount = scanMilk.nextInt();
+
+        Scanner scanCoffeeBeans = new Scanner(System.in);
+        System.out.print("\nWrite how many grams of coffee beans the coffee machine has:\n" + "> ");
+        int coffeeBeansAmount = scanCoffeeBeans.nextInt();
+
+        Scanner scanAmountOfCups = new Scanner(System.in);
+        System.out.print("\nWrite how many cups of coffee you will need:\n" + "> ");
+        int cupsAmount = scanAmountOfCups.nextInt();
+
+    }
 }
