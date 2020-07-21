3594719a2d0c502a7f3dc9a58dcdbc5342a96cb3
ArmineZak <armzak91@gmail.com>
2020-07-22 00:05:10 +0400

    - added method fill

--- virtual-coffee-machine/src/CoffeeLoop.java
+++ virtual-coffee-machine/src/CoffeeLoop.java
@@ -1,13 +1,38 @@
 import java.util.Scanner;
 
 public class CoffeeLoop {
+    public static Scanner sc = new Scanner(System.in);
+    public static int milk = 400;
+    public static int water = 540;
+    public static int coffee = 120;
+    public static int cups = 9;
+    public static int cash = 550;
+
     public static void main(String[] args) {
 
-        public static Scanner sc = new Scanner(System.in);
-        public static int milk = 400;
-        public static int water = 540;
-        public static int coffee = 120;
-        public static int cups = 9;
-        public static int cash = 550;
+
     }
+
+    //this method fills ingredients from input and gives the remaining amount
+    public static void fill() {
+
+        System.out.print("Fill water: \n" + "> ");
+        int fillWater = sc.nextInt();
+        water += fillWater;
+
+        System.out.print("Fill milk: \n" + "> ");
+        int fillMilk = sc.nextInt();
+        milk += fillMilk;
+
+        System.out.print("Fill coffee: \n" + "> ");
+        int fillCoffee = sc.nextInt();
+        coffee += fillCoffee;
+
+        System.out.println("Machine has\n " +
+                water + " ml water\n" +
+                milk + " ml milk\n" +
+                coffee + " g coffee\n" +
+                cash + "$\n");
+    }
+
 }
