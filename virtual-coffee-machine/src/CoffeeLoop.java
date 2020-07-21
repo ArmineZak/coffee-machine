6f069cb9c3df959812bdaa17f54c47eef93b009a
ArmineZak <armzak91@gmail.com>
2020-07-22 00:00:08 +0400

    - added Scanner and variables for ingredients

---
+++ virtual-coffee-machine/src/CoffeeLoop.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class CoffeeLoop {
+    public static void main(String[] args) {
+
+        public static Scanner sc = new Scanner(System.in);
+        public static int milk = 400;
+        public static int water = 540;
+        public static int coffee = 120;
+        public static int cups = 9;
+        public static int cash = 550;
+    }
+}
