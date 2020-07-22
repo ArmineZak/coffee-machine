50464799a3ed1d5b4f03e1e93ff6b1fc9e3eed0c
ArmineZak <armzak91@gmail.com>
2020-07-23 02:30:43 +0400

    - replaced while true with smth different

--- virtual-coffee-machine/src/CoffeeLoop.java
+++ virtual-coffee-machine/src/CoffeeLoop.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class CoffeeLoop {
 
     public static void main(String[] args) {
+        String userInput = "userInput";
 
-        while (true) {
+        while (!"exit".equals(userInput)) {
             System.out.println("Write action buy, fill, take, remaining, exit:");
             Scanner input = new Scanner(System.in);
-            String userInput = input.nextLine();
+            userInput = input.nextLine();
 
             if ("exit".equals(userInput)) {
                 break;
