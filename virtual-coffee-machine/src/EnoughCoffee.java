e878f66d4ae812648b935962493ee6bbe5f65563
ArmineZak <armzak91@gmail.com>
2020-07-24 12:39:26 +0400

    - decoupled into 2 parts - added method to calculate the amount of cups that can be made - added comments

--- virtual-coffee-machine/src/EnoughCoffee.java
+++ virtual-coffee-machine/src/EnoughCoffee.java
@@ -3,14 +3,14 @@ import java.lang.Math;
 
 public class EnoughCoffee {
 
+    private static int waterForOneCup = 200;
+    private static int milkForOneCup = 50;
+    private static int coffeeBeansForOneCup = 15;
+
+
     public static void main(String[] args) {
 
-        int waterForOneCup = 200;
-        int milkForOneCup = 50;
-        int coffeeBeansForOneCup = 15;
-
-
-        //using scanner to input necessary amount of ingredients
+        //using scanner to input ingredients
         Scanner sc = new Scanner(System.in);
         System.out.println("Write how many ml of water the coffee machine has:");
         int waterAmount = sc.nextInt();
@@ -24,17 +24,19 @@ public class EnoughCoffee {
         System.out.println("Write how many cups of coffee you will need:");
         int cupsAmount = sc.nextInt();
 
+        calculateCups(waterAmount, milkAmount,coffeeBeansAmount,cupsAmount);
+    }
 
-        //using Math.min to find the min possible amount of cups for each variety of coffee
+
+    //using Math.min to find the min possible amount of cups for each variety of coffee
+    //checking cases for possible amount of cups that can be made
+    public static void calculateCups(int waterAmount, int milkAmount, int coffeeBeansAmount, int cupsAmount) {
 
         int minWater = waterAmount / waterForOneCup;
         int minMilk = milkAmount / milkForOneCup;
         int minCoffeeBeans = coffeeBeansAmount / coffeeBeansForOneCup;
-
         int min = Math.min(minWater, Math.min(minMilk, minCoffeeBeans));
 
-
-        //checking cases for possible amount of cups to be made
 
         if (cupsAmount == min || cupsAmount < min) {
             if (cupsAmount == min) {
@@ -46,8 +48,8 @@ public class EnoughCoffee {
 
         } else if (cupsAmount > min) {
             System.out.println("No, I can make only " + min + " cup(s) of coffee.");
-
         }
     }
 }
+
 
